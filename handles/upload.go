4cbfbe69643a0fe9a0c9d000a01c7801c19ba7f5
zhangbaqing <zhangbaqing@xiaomi.com>
2020-04-01 09:55:06 +0800

    [GHzbq]增加本地和远端baseURL

--- handles/upload.go
+++ handles/upload.go
@@ -13,7 +13,8 @@ import (
 
 const (
 	imagePath = "/home/work/project/file_server/img/"
-	serverBase = "http://47.102.208.185:9653"
+	//serverBase = "http://47.102.208.185:9653"
+	serverBase = "http://127.0.0.1:9653"
 )
 
 var (
